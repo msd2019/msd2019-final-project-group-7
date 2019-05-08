0c90d2ee952050d546d8bf6f20d39f5cddaced8d
Nonie Thomas <43184637+noniethomas@users.noreply.github.com>
2019-05-08 11:34:48 -0400

    Update 01_get_original_data.sh

--- 01_get_original_data.sh
+++ 01_get_original_data.sh
@@ -1,5 +1,4 @@
 # download the zipfile
-#[ -f 2007-0024_data.zip ] || curl -b cookies.txt -L -O https://www.aeaweb.org/aej/pol/data/2007-0024_data.zip
 curl -b cookies.txt -L https://www.aeaweb.org/aej/pol/data/2007-0024_data.zip -o 2007-0024_data.zip
 
 # unzip it
