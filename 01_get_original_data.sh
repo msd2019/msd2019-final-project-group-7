69cb87340b46f8d41c1c504a1775120dc45c6536
Nonie Thomas <43184637+noniethomas@users.noreply.github.com>
2019-05-09 22:50:47 -0400

    Update 01_get_original_data.sh

--- 01_get_original_data.sh
+++ 01_get_original_data.sh
@@ -2,4 +2,11 @@
 curl -b cookies.txt -L https://www.aeaweb.org/aej/pol/data/2007-0024_data.zip -o 2007-0024_data.zip
 
 # unzip it
-unzip 2007-0024_data.zip 
+unzip 2007-0024_data.zip
+
+# remove authors code
+rm -rf PisoFirme-MATA-MoransI.do
+rm -rf PisoFirme_AEJPol-20070024_STATA-replication-code.do
+
+# remove readme
+rm -rf README.pdf
